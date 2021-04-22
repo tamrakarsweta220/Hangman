369b0225190b894d7d1507fb0edb3869a791db63
tamrakarsweta <tamrakarsweta220@gmail.com>
2021-04-22 03:45:56 -0400

    end game working but need to end client loop

--- class files/Client.java
+++ class files/Client.java
@@ -7,7 +7,6 @@ class Client {
 
         String userInput;
         String serverResponse;
-        String serverResponse2;
         String host = argv[0];
         int port = (new Integer(argv[1])).intValue();
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
@@ -19,67 +18,73 @@ class Client {
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
         System.out.println("Welcome to the hangman game!");
-        System.out.println("If you are the creator, enter creator. If you are the guesser, enter guesser.");
-        System.out.println("creator or guesser");
+        System.out.println("If you are the creator, please type in creator. If you are the guesser, please type in guesser. So, are you a creator or a guesser?");
         String player = inFromUser.readLine();
-        System.out.println("The player is a: " + player);
+        System.out.println("The player is a: " + player+".");
         outToServer.writeBytes(player + "\n");
-        System.out.println("sent playr info to server");
+        System.out.println("Client sent the player info to the server. Please wait to get a response from server.");
 
         if (player.equals("creator")) {
             serverResponse = inFromServer.readLine();
-            System.out.println("got player related respond from server");
+            System.out.println("Creator got the prompt for writing hangmanWord to the server.\n");
             System.out.println(serverResponse);
 
             String hangmanWord = inFromUser.readLine();
-            System.out.println("got hangmanWord from user: " + hangmanWord);
+            System.out.println("Creator got the hangmanWord from the user: " + hangmanWord);
             outToServer.writeBytes(hangmanWord + "\n");
+            System.out.println("Creator sent "+hangmanWord+" to the server.\n");
+
+            serverResponse = inFromServer.readLine();
+            System.out.println("Creator got the prompt for writing dashedWord to the server.");
+            System.out.println(serverResponse);
+
+            String dashedWord = inFromUser.readLine();
+            System.out.println("Creator got the dashedWord from user: " + dashedWord+".\n");
+            outToServer.writeBytes(dashedWord + "\n");
+            System.out.println("Creator sent the dashedWord to the server.\n");
+
             while (true) {
 
-                System.out.println("This is where the while loop for creator starts");
-                System.out.println("now waiting to get something from server first");
+                System.out.println("This is where the while loop for creator starts. So, we are now waiting to get something from the server first.");
 
                 serverResponse = inFromServer.readLine();
-                System.out.println("got the serverResponse");
-
+                System.out.println("Creator got the response from server.\n");
                 System.out.println(serverResponse);
 
-                System.out.println("getting input from user");
+                System.out.println("Creator is waiting to get the modified progress word from user.");
                 userInput = inFromUser.readLine();
-                System.out.println("got input from user and sending that to server");
+                System.out.println("Creator got input from user and is now sending that to the server.");
 
                 outToServer.writeBytes(userInput + "\n");
-                System.out.println("sent the input to the server");
+                System.out.println("Creator sent the input to the server.\n");
             }
         }
         if (player.equals("guesser")) {
             serverResponse = inFromServer.readLine();
-            System.out.println("got player related respond from server");
+            System.out.println("Guesser got the prompt for writing if they are ready to the server.\n");
             System.out.println(serverResponse);
 
-            String waiting = inFromUser.readLine();
-            System.out.println("got waiting confirmation: " + waiting);
-            outToServer.writeBytes(waiting + "\n");
+            String ready = inFromUser.readLine();
+            System.out.println("Guesser received the ready confirmation from user: " + ready);
+            outToServer.writeBytes(ready + "\n");
+            System.out.println("Guesser sent the ready confirmation to the server.\n");
 
             serverResponse = inFromServer.readLine();
-            System.out.println("got the guessign word from server. Now start playing");
+            System.out.println("Guesser got the dashedWord from the server. Now guesser can start playing by guessing individual letters.");
             System.out.println(serverResponse);
             while (true) {
 
-                System.out.println("This is where the while loop for guesser starts");
-                System.out.println("please enter a letter");
+                System.out.println("This is where the while loop for guesser starts. Now, please start guessign by typing in a letter.\n");
 
-                System.out.println("getting input from user");
+                System.out.println("Guesser is waiting to get the letter from user.");
                 userInput = inFromUser.readLine();
-                System.out.println("got input from user and sending that to server");
+                System.out.println("Guesser got the letter from user and is now sending that to the server.");
 
                 outToServer.writeBytes(userInput + "\n");
-                System.out.println("sent the input to the server");
+                System.out.println("Guesser sent the letter to the server. Now, it is waiting to hear if their guess was correct or incorrect.\n");
 
-                System.out.println("now waiting to get something from server first");
                 serverResponse = inFromServer.readLine();
-                System.out.println("got the serverResponse");
-
+                System.out.println("Guesser got the response (correct or incorrect) from the creator.\n");
                 System.out.println(serverResponse);
             }
         }
--- class files/ServerHandler.java
+++ class files/ServerHandler.java
@@ -6,16 +6,20 @@ public class ServerHandler implements Runnable {
     Socket socket1;
     Socket socket2;
     String hangmanWord;
+    String dashedWord;
+    String progress; //new modified word
+    String progressTracker; //previous progress made
     String waiting;
     String player1;
     String player2;
     BufferedReader client1br;
     BufferedReader client2br;
+    boolean gameOn;
     // Constructor
     public ServerHandler(Socket socket1,Socket socket2) throws Exception {
         this.socket1 = socket1;
         this.socket2 = socket2;
-        hangmanWord="";
+        gameOn=true;
     }
     // Implement the run() method of the Runnable interface.
     public void run() {
@@ -37,24 +41,27 @@ public class ServerHandler implements Runnable {
         client1br = new BufferedReader(new InputStreamReader(is1));
         client2br = new BufferedReader(new InputStreamReader(is2)); 
 
-        while (true) {
-            System.out.println("This is where the while loop for server starts");
+        while (gameOn) {
+            System.out.println("This is where the while loop for server starts.\n");
 
                 // Get the incoming message from the client (read from socket)
                 String msg1 = client1br.readLine();
                 String msg2 = client2br.readLine();
-                if(msg1.equals("creator")){
-                    System.out.println("sending message to client1");
-                    os1.writeBytes("Please give a word to the guesser. The word can have as many letters and as many dashes as you wish. Eg: K--N--R-- for KANGAROO\r\n");
+                if(msg1.toLowerCase().equals("creator")){
+                    System.out.println("Server is sending message to client1.\n");
+                    os1.writeBytes("Please give a word for the hangman game. Eg: KANGAROO\r\n");
                     hangmanWord = client1br.readLine();
-                    System.out.println("Hangman word is: "+hangmanWord);
+                    System.out.println("Hangman word is: "+hangmanWord+".\n");
+                    os1.writeBytes("Place some dashes in the word for the guesser to guess the word. The word can have as many letters and as many dashes as you wish. Eg: K--N--R-- for KANGAROO\r\n");
+                    dashedWord = client1br.readLine();
+                    progress=dashedWord; //to help automate the process of calculating the correct or incorrect guess
                 }
-                if(msg2.equals("guesser")){
-                    System.out.println("sending message to client2");
-                    os2.writeBytes("Wait to get a word. Say ok if you're ready.\r\n");
+                if(msg2.toLowerCase().equals("guesser")){
+                    System.out.println("Server is sending message to client2.\n");
+                    os2.writeBytes("The creator is going to send you a word to guess. Say ok if you're ready to play the game.\r\n");
                     waiting = client2br.readLine();
-                    System.out.println("received a ready response from guesser");
-                    os2.writeBytes("You need to guess: "+hangmanWord+"\r\n"); 
+                    System.out.println("The guesser said that they are ready to play the game.\n");
+                    os2.writeBytes("You need to guess: "+dashedWord+". Please start by entering a letter.\r\n"); 
                 }
 
                 /**
@@ -65,29 +72,50 @@ public class ServerHandler implements Runnable {
                  * Then, we will read what teh creator sent and send it to the guesser
                  * and ask the guesser to enter a letter again (in the loop).
                 */
-                for(int i=0; i<6; i++){
-                    System.out.println("reading the letter sent by guesser.");
+                for(int i=0; i<hangmanWord.length()-1; i++){
+                    System.out.println("Server is reading the letter sent by guesser.\n");
                     String letter2 = client2br.readLine();
                     System.out.println(letter2);
-                    System.out.println("sending the letter guessed by guesser to creator");
-                    os1.writeBytes("Client 2 sent this letter: "+letter2+"Is it a correct guess? If it is, replace the dashes with the letter. If not, give the same word from before.");
-                    System.out.println("sent the letter guessed to creator");
-                    String progress = client1br.readLine();
+                    System.out.println("Server is sending the letter guessed by guesser to creator.");
+                    os1.writeBytes("Client 2 guessed this letter: "+letter2+". Is it a correct guess? If it is, replace the dashes with the letter. If not, give the same word from before."+ "\n");
+                    System.out.println("Server sent the letter guessed by guesser to the creator.\n");
+                    progressTracker=progress;
+                    progress = client1br.readLine();
                     System.out.println(progress);
-                    os2.writeBytes("Your guess was correct/incorrect. The progress so far is: "+progress+"please enter another letter");
+                    if(progress.equals(hangmanWord)){
+                        os2.writeBytes("Yay you win! You guessed the hangman word correctly: "+hangmanWord+"\n");
+                        System.out.println("Game won.\n");
+                        //write something to client 1 to indicate game end?
+                        break;
+                    }else if(progress.equals(progressTracker)){
+                        os2.writeBytes("Your guess was incorrect. The progress so far is: "+progress+". Please enter another letter."+ "\n");
+                    }else{
+                        os2.writeBytes("Your guess was correct. The progress so far is: "+progress+". Please enter another letter."+ "\n");
+                    }
                 }
+
+                System.out.println("End of game");
+
+                //close all streams and sockets
+                socket1.close();
+                System.out.println("socket1 is closed.");
+                socket2.close();
+                System.out.println("socket 2 is closed.");
+                client1br.close();
+                System.out.println("buffer reader 1 is closed.");
+                client2br.close();
+                System.out.println("buffer reader 2 is closed.");
+                os1.close();
+                System.out.println("output stream 1 is closed.");
+                os2.close();
+                System.out.println("output stream 2 is closed.");
+
+                gameOn=false;
         }
     }
-    // private String responses(BufferedReader client, String clientInput){
-    //     if(client==client1br){
-    //         if(clientInput.toLowerCase().equals("creator")){
-    //             return "Please give a word to the guesser. The word can have as many letters and as many dashes as you wish. Eg: K--N--R-- for KANGAROO\r\n";
-    //         }
-    //         hangmanWord=clientInput;
-    //         return "Great! The word has been sent to guesser";
-    //     }else if(client==client2br && clientInput.toLowerCase().equals("guesser")){
-    //         return "The word that you have to guess is: "+hangmanWord;
-    //     }
-    //     return "Server response:"+clientInput.toUpperCase();
-    // }
 }
+
+//question 1: do we have to close input stream as well?
+//question 2: how to end the game? disconnected server and client connection
+//            but having problem with teh loop satrted in client class.
+//            It is trying to complete the while loop there.
